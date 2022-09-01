766f40fe089069714f488011131a93e8f5384928
tarunrathore123 <31217259+tarunrathore123@users.noreply.github.com>
2022-09-01 17:47:37 +0530

    implemented header

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,3 +1,10 @@
+import { useRef, useState } from "react";
+import Header from "../../components/header";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
